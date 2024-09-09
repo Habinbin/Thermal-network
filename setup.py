c7721da0787388fe775e7a34db8a06d10eba865c
Habinzzang <habinjo0608@gmail.com>
2024-09-09 14:35:02 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="thermal_simulation",
-    version="0.2",
+    version="0.3",
     packages=find_packages(),
     install_requires=[
         "numpy",
