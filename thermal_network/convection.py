97a0756163b17f821f44324b3421f81f41f3366c
Habin Jo <habinjo0608@gmail.com>
2024-09-16 22:57:23 +0900

    convection

---
+++ thermal_network/convection.py
@@ -0,0 +1,22 @@
+import math
+from datetime import datetime
+import constant as c
+
+
+# exeterior convection coefficient 
+def simple_combined_convection(roughness, Vz):
+    roughness_values = {
+    1: (11.58, 5.894, 0.0),
+    2: (12.49, 4.065, 0.028),
+    3: (10.79, 4.192, 0.0),
+    4: (8.23, 4.0, -0.057),
+    5: (10.22, 3.1, 0.0),
+    6: (8.23, 3.33, -0.036)
+    }
+    if roughness in roughness_values:
+        D, E, F = roughness_values[roughness]
+        h_co = D + E*Vz + F*Vz**2
+    else:
+        raise ValueError("Invalid roughness value")
+    
+    return h_co
--- thermal_network/core.py
+++ thermal_network/core.py
@@ -21,22 +21,46 @@ class SetConstruction:
 
     def dx(self):
         dx = np.array([self.layer_lst[Lidx].dx for Lidx in range(self.lN) for _ in range(self.div_lst[Lidx])])  # dx [m]
+        return dx
+    
+    def dx_L(self):
+        dx = np.array([self.layer_lst[Lidx].dx for Lidx in range(self.lN) for _ in range(self.div_lst[Lidx])])  # dx [m]
         dx_L = np.array([dx[0]/2] + [(dx[i-1] + dx[i])/2 for i in range(1, self.N)])  
+        return dx_L
+    
+    def dx_R(self):
+        dx = np.array([self.layer_lst[Lidx].dx for Lidx in range(self.lN) for _ in range(self.div_lst[Lidx])])  # dx [m]
         dx_R = np.array([(dx[i] + dx[i+1])/2 for i in range(self.N-1)] + [dx[self.N-1]/2]) 
-        return dx, dx_L, dx_R
+        return dx_R
 
     def K(self):
         R = np.array([self.layer_lst[Lidx].R for Lidx in range(self.lN) for _ in range(self.div_lst[Lidx])])  # Thermal resistance [m^2K/W]
         K = 1/R  # Thermal conductance [W/m^2K]  
+        return K
+    
+    def K_L(self):
+        R = np.array([self.layer_lst[Lidx].R for Lidx in range(self.lN) for _ in range(self.div_lst[Lidx])])  # Thermal resistance [m^2K/W]
         K_L = 1/np.array([R[0]/2] + [(R[i-1] + R[i])/2 for i in range(1, self.N)])  # Left interface thermal conductance [W/m^2K]
+        return K_L
+    
+    def K_R(self):
+        R = np.array([self.layer_lst[Lidx].R for Lidx in range(self.lN) for _ in range(self.div_lst[Lidx])])  # Thermal resistance [m^2K/W]
         K_R = 1/np.array([(R[i] + R[i+1])/2 for i in range(self.N-1)] + [R[self.N-1]/2])  # Right interface thermal conductance [W/m^2K]
-        return K, K_L, K_R
+        return K_R
     
     def R(self):
         R = np.array([self.layer_lst[Lidx].R for Lidx in range(self.lN) for _ in range(self.div_lst[Lidx])])  # Thermal resistance [m^2K/W]
+        return R
+    
+    def R_L(self):
+        R = np.array([self.layer_lst[Lidx].R for Lidx in range(self.lN) for _ in range(self.div_lst[Lidx])])  # Thermal resistance [m^2K/W]
         R_L = np.array([R[0]/2] + [(R[i-1] + R[i])/2 for i in range(1, self.N)])  # Left interface thermal resistance [m^2K/W]
+        return R_L
+    
+    def R_R(self):
+        R = np.array([self.layer_lst[Lidx].R for Lidx in range(self.lN) for _ in range(self.div_lst[Lidx])])  # Thermal resistance [m^2K/W]
         R_R = np.array([(R[i] + R[i+1])/2 for i in range(self.N-1)] + [R[self.N-1]/2])  # Right interface thermal resistance [m^2K/W]
-        return R, R_L, R_R
+        return R_R
     
     def K_tot(self):
         R = np.array([self.layer_lst[Lidx].R for Lidx in range(self.lN) for _ in range(self.div_lst[Lidx])])  # Thermal resistance [m^2K/W]
@@ -82,8 +106,8 @@ def eliminate_pre_simulation_data(data, pre_simulation_time_step):
 
 def TDMA(Construction, T, T_L, T_R, t):
     N = Construction.N
-    dx, dx_L, dx_R = Construction.dx()
-    K, K_L, K_R = Construction.K()
+    dx, = Construction.dx()
+    K_L, K_R = Construction.K_L(), Construction.K_R()
     C = Construction.C()
     a_list = [-t*K_L[i] for i in range(N)]
     b_list = [2*dx[i]*C[i]+t*K_L[i]+t*K_R[i] for i in range(N)]
