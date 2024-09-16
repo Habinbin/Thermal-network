2cba8b54c48e3fb9455d8f51d81be060179f84a1
Habinzzang <habinjo0608@gmail.com>
2024-09-17 00:24:52 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="thermal_simulation",
+    name="thermal_network",
     version="0.3",
     packages=find_packages(),
     install_requires=[
